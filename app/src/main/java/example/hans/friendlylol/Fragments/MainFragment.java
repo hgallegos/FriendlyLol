a289d64d95d842564069691dc53b00e0bdd49495
hgallegos <hgallego@alumnos.ubiobio.cl>
2016-06-19 18:31:50 -0400

    Probando commit desde AndroidStudio

---
+++ app/src/main/java/example/hans/friendlylol/Fragments/MainFragment.java
@@ -0,0 +1,9 @@
+package example.hans.friendlylol.Fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by hans6 on 19-06-2016.
+ */
+public class MainFragment extends Fragment {
+}
