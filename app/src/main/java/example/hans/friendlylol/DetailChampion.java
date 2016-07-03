3dddfa4f1550d11a6c80b040b0f9fcfffd7c9688
Matias <maty_owen11@hotmail.com>
2016-07-03 04:14:18 -0300

    modificando detalle de campeones (se agrego collapsin toolbar y  scrpllable tabs)

--- app/src/main/java/example/hans/friendlylol/DetailChampion.java
+++ app/src/main/java/example/hans/friendlylol/DetailChampion.java
@@ -1,13 +1,26 @@
 package example.hans.friendlylol;
 
+import android.annotation.SuppressLint;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.robrua.orianna.api.core.AsyncRiotAPI;
@@ -15,6 +28,11 @@ import com.robrua.orianna.type.api.Action;
 import com.robrua.orianna.type.core.common.Region;
 import com.robrua.orianna.type.core.staticdata.Champion;
 import com.robrua.orianna.type.exception.APIException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import example.hans.friendlylol.model.VersionModel;
 
 public class DetailChampion extends AppCompatActivity {
     private Champion champion;
@@ -37,6 +55,15 @@ public class DetailChampion extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
+        //  TABS
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.htab_viewpager);
+        setupViewPager(viewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.htab_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        //  FIN-TABS
+
+
         TypedValue typedValueColorPrimaryDark = new TypedValue();
         DetailChampion.this.getTheme().resolveAttribute(R.attr.colorPrimaryDark, typedValueColorPrimaryDark, true);
         final int colorPrimaryDark = typedValueColorPrimaryDark.data;
@@ -44,7 +71,7 @@ public class DetailChampion extends AppCompatActivity {
             getWindow().setStatusBarColor(colorPrimaryDark);
         }
 
-       Bundle bundle = getIntent().getExtras();
+        Bundle bundle = getIntent().getExtras();
         if(bundle != null){
             AsyncRiotAPI.setAPIKey(API_KEY);
             AsyncRiotAPI.setRegion(Region.LAS);
@@ -71,5 +98,120 @@ public class DetailChampion extends AppCompatActivity {
             }, bundle.getLong("championID"));
         }
 
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                switch (tab.getPosition()) {
+                    case 0:
+                        Toast.makeText(getApplicationContext(), "1", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+                        Toast.makeText(getApplicationContext(), "2", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2:
+                        Toast.makeText(getApplicationContext(), "3", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 3:
+                        Toast.makeText(getApplicationContext(), "4", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 4:
+                        Toast.makeText(getApplicationContext(), "5", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+    }//fin onCreate
+
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new DummyFragment(getResources().getColor(R.color.accent_material_light)), "Resumen");
+        adapter.addFrag(new DummyFragment(getResources().getColor(R.color.ripple_material_light)), "Oponentes & Tipps");
+        adapter.addFrag(new DummyFragment(getResources().getColor(R.color.button_material_dark)), "Habilidades");
+        adapter.addFrag(new DummyFragment(getResources().getColor(R.color.accent_material_dark)), "Builds");
+        adapter.addFrag(new DummyFragment(getResources().getColor(R.color.ripple_material_dark)), "Skins");
+        viewPager.setAdapter(adapter);
     }
-}
+
+    static class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFrag(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+    public static class DummyFragment extends Fragment {
+        int color;
+//        SimpleRecyclerAdapter adapter;
+
+        public DummyFragment() {
+        }
+
+        @SuppressLint("ValidFragment")
+        public DummyFragment(int color) {
+            this.color = color;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.dummy_fragment, container, false);
+
+            final FrameLayout frameLayout = (FrameLayout) view.findViewById(R.id.dummyfrag_bg);
+            frameLayout.setBackgroundColor(color);
+
+            RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.dummyfrag_scrollableview);
+
+            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity().getBaseContext());
+            recyclerView.setLayoutManager(linearLayoutManager);
+            recyclerView.setHasFixedSize(true);
+
+            List<String> list = new ArrayList<String>();
+            for (int i = 0; i < VersionModel.data.length; i++) {
+                list.add(VersionModel.data[i]);
+            }
+
+//            adapter = new SimpleRecyclerAdapter(list);
+//            recyclerView.setAdapter(adapter);
+            return view;
+        }
+    }
+
+
+
+}//fin
